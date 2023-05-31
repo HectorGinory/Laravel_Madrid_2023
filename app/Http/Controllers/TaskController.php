292775f21f21616338ae697943b8637d80278abf
HectorGinory <hectorginorydevs@gmail.com>
2023-05-31 13:50:01 +0200

    Test

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -139,7 +139,7 @@ class TaskController extends Controller
                         'success' => true,
                         'message' => "task doesnt exists"
                     ]
-                    , 
+                    ,
                     404
                 );
             }
